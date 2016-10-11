74abbc00e413e042c4f9d487332f75d216d12d67
lagos tout <lagos.tout@gmail.com>
2016-10-11 15:00:34 -0700

    P7 Temperature Tracker - Solved!
    https://www.interviewcake.com/question/java/temperature-tracker

--- src/main/groovy/com/lagostout/interviewcake/TemperatureTracker.groovy
+++ src/main/groovy/com/lagostout/interviewcake/TemperatureTracker.groovy
@@ -1,4 +1,43 @@
 package com.lagostout.interviewcake
 
+import static org.apache.commons.lang3.Range.between
+import org.apache.commons.lang3.Range
+
+// https://www.interviewcake.com/question/java/temperature-tracker
+
 class TemperatureTracker {
+
+    Range<Integer> tempRange = between(0,110)
+    int min = tempRange.maximum + 1
+    int max = tempRange.minimum - 1
+    int[] frequency = new int[tempRange.maximum + 1]
+    int mode
+    int count
+    int sum
+
+    public void insert(int temp) {
+        max = Math.max(temp, max)
+        min = Math.min(temp, min)
+        frequency[temp] += 1
+        mode = frequency[temp] >= frequency[mode] ? temp : mode
+        count++
+        sum += temp
+    }
+
+    public int getMax() {
+        max
+    }
+
+    public int getMin() {
+        min
+    }
+
+    public double getMean() {
+        (sum as double) / count
+    }
+
+    public int getMode() {
+        mode
+    }
+
 }
--- src/test/groovy/com/lagostout/interviewcake/TemperatureTrackerSpec.groovy
+++ src/test/groovy/com/lagostout/interviewcake/TemperatureTrackerSpec.groovy
@@ -1,4 +1,36 @@
 package com.lagostout.interviewcake
 
-class TemperatureTrackerSpec {
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class TemperatureTrackerSpec extends Specification {
+
+    TemperatureTracker tracker
+
+    def setup() {
+        tracker = new TemperatureTracker()
+    }
+
+    @Unroll('#temps #expected')
+    def 'track temperature'(List<Integer> temps, List<Number> expected) {
+
+        when:
+        temps.each tracker.&insert
+
+        then:
+        tracker.getMax() == expected[0]
+        tracker.getMin() == expected[1]
+        tracker.getMean() == expected[2]
+        tracker.getMode() == expected[3]
+
+        where:
+        [temps, expected] << [
+                [[1], [1,1,1,1]],
+                [[1,2], [2,1,1.5,2]],
+                [[2,1,1,3,0,5], [5,0,2,1]],
+                [[1,1,1,2,2,2,2,1,1,1], [2,1,1.4,1]]
+        ]
+
+    }
+
 }
