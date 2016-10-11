2448cb8274fb34574f24034a9e2b0d6042a34cde
lagos tout <lagos.tout@gmail.com>
2016-10-11 15:43:41 -0700

    P8 Balanced Binary Tree - Solved!
    https://www.interviewcake.com/question/balanced-binary-tree

--- src/main/groovy/com/lagostout/interviewcake/BalancedBinaryTree.groovy
+++ src/main/groovy/com/lagostout/interviewcake/BalancedBinaryTree.groovy
@@ -1,4 +1,79 @@
 package com.lagostout.interviewcake
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder
+import org.apache.commons.lang3.builder.ToStringStyle
+
+// https://www.interviewcake.com/question/java/balanced-binary-tree
+
 class BalancedBinaryTree {
+
+    static boolean isBalanced(BinaryTreeNode binaryTreeNode) {
+        LinkedList<UnexploredNode> unexploredNodes = new LinkedList<>()
+        int minLeafDepth = Integer.MAX_VALUE
+        int maxLeafDepth = Integer.MIN_VALUE
+        boolean isBalanced = true
+        boolean foundLeaf = false
+        unexploredNodes.push(new UnexploredNode(binaryTreeNode, 0))
+        while (!unexploredNodes.isEmpty()) {
+            UnexploredNode unexploredNode = unexploredNodes.pop()
+            int currentDepth = unexploredNode.level
+            BinaryTreeNode node = unexploredNode.node
+            if (node.right == null && node.left == null) {
+                minLeafDepth = Math.min(minLeafDepth, currentDepth)
+                maxLeafDepth = Math.max(maxLeafDepth, currentDepth)
+                isBalanced = maxLeafDepth - minLeafDepth <= 1
+                foundLeaf = true
+            } else {
+                int depth = currentDepth + 1
+                if (node.right != null) {
+                    unexploredNodes.push(new UnexploredNode(node.right, depth))
+                }
+                if (node.left != null) {
+                    unexploredNodes.push(new UnexploredNode(node.left, depth))
+                }
+                if (foundLeaf)
+                    isBalanced = depth - maxLeafDepth <= 1
+            }
+            if (!isBalanced) break
+        }
+        isBalanced
+    }
+
+    static class UnexploredNode {
+        BinaryTreeNode node
+        int level
+
+        UnexploredNode(BinaryTreeNode node, int level) {
+            this.node = node
+            this.level = level
+        }
+    }
+
+    static class BinaryTreeNode {
+
+        int value
+        BinaryTreeNode left
+        BinaryTreeNode right
+
+        BinaryTreeNode(int value) {
+            this.value = value
+        }
+
+        BinaryTreeNode insertLeft(int leftValue) {
+            this.left = new BinaryTreeNode(leftValue)
+            return this.left
+        }
+
+        BinaryTreeNode insertRight(int rightValue) {
+            this.right = new BinaryTreeNode(rightValue)
+            return this.right
+        }
+
+        @Override
+        String toString() {
+            return new ReflectionToStringBuilder(
+                    this, ToStringStyle.SHORT_PREFIX_STYLE).toString()
+        }
+    }
+
 }
--- src/test/groovy/com/lagostout/interviewcake/BalancedBinaryTreeSpec.groovy
+++ src/test/groovy/com/lagostout/interviewcake/BalancedBinaryTreeSpec.groovy
@@ -1,4 +1,96 @@
 package com.lagostout.interviewcake
 
-class BalancedBinaryTreeSpec {
+import spock.lang.Specification
+import com.lagostout.interviewcake.BalancedBinaryTree.BinaryTreeNode
+import spock.lang.Unroll
+
+class BalancedBinaryTreeSpec extends Specification {
+
+    @Unroll('#node #expected')
+    def 'is balanced'(BinaryTreeNode node, boolean expected) {
+        expect:
+        BalancedBinaryTree.isBalanced(node) == expected
+        where:
+        [node, expected] << [
+                [singleNodeBinaryTree(), true],
+                [unbalancedBinaryTree1(), false],
+                [exactlyBalancedBinaryTree1(), true],
+                [inexactlyBalancedBinaryTree1(), true]
+        ]
+    }
+
+    BinaryTreeNode singleNodeBinaryTree() {
+        new BinaryTreeNode(1)
+    }
+
+    BinaryTreeNode unbalancedBinaryTree1() {
+        BinaryTreeNode node = new BinaryTreeNode(1)
+        node.insertLeft(2).insertRight(3).insertRight(4)
+        node.insertRight(5)
+        node
+    }
+
+    BinaryTreeNode unbalancedBinaryTree2() {
+        BinaryTreeNode node = new BinaryTreeNode(1)
+        int value = 1
+
+        node.insertLeft(value++)
+        node.left.with {
+            it.insertLeft(value++)
+            it.insertRight(value++)
+        }
+        node.insertRight(value++)
+        node.right.with {
+            it.insertLeft(value++)
+            it.insertRight(value++)
+        }
+
+        node.right.insertLeft(value++)
+        node.right.left.with {
+            it.insertLeft(value++)
+            it.insertRight(value++)
+        }
+        node.right.insertRight(value++)
+        node.right.right.with {
+            it.insertLeft(value++)
+            it.insertRight(value++)
+        }
+
+        node.left.insertLeft(value++)
+        node.left.left.with {
+            it.insertLeft(value++)
+            it.insertRight(value++)
+        }
+        node.left.insertRight(value++)
+        node.left.right.with {
+            it.insertLeft(value++)
+            it.insertRight(value++)
+        }
+    }
+
+    BinaryTreeNode exactlyBalancedBinaryTree1() {
+        BinaryTreeNode node = new BinaryTreeNode(1)
+        node.insertLeft(2)
+        node.insertRight(3)
+        with (node.left) {
+            it.insertLeft(4)
+            it.insertRight(5)
+        }
+        with (node.right) {
+            it.insertLeft(6)
+            it.insertRight(7)
+        }
+        node
+    }
+
+    BinaryTreeNode inexactlyBalancedBinaryTree1() {
+        BinaryTreeNode node = new BinaryTreeNode(1)
+        node.insertLeft(2)
+        node.insertRight(3)
+        with (node.left) {
+            it.insertLeft(4)
+            it.insertRight(5)
+        }
+        node
+    }
 }
